b58a1f2e5e81545bea7d900c48992903e723c887
Filipe <filipesilveira.dev@gmail.com>
2025-09-02 21:24:34 -0300

    Duplo clique resolvido

--- script.js
+++ script.js
@@ -3,36 +3,36 @@
 function toCreateSection(){
     const toCreateSectionBtn = document.getElementById("createBtn");
     const showCreateSection = document.getElementById("createSection");
-    toCreateSectionBtn.addEventListener("click", ()=>{
+    
 
         const hideLandingSection = document.getElementById("landingSection");
         hideLandingSection.style.display = "none";
         showCreateSection.style.display = "flex";
-    })
+    
 }
 
 //função criada para esconder a landing page e "ir" para a seção de lista
 function toListSection(){
     const toListSectionBtn = document.getElementById("listBtn");
     const showListSection = document.getElementById("ListSection");
-    toListSectionBtn.addEventListener("click", ()=>{
+    
         
         const hideLandingSection = document.getElementById("landingSection");
         hideLandingSection.style.display = "none";
         showListSection.style.display = "flex";
-    })
+   
 }
 
 //função criada para esconder a landing page e "ir" para a seção de deletar
 function toDeleteSection(){
     const toDeleteSectionBtn = document.getElementById("deleteBtn");
     const showDeleteSection = document.getElementById("deleteSection");
-    toDeleteSectionBtn.addEventListener("click", ()=>{
+    
         
         const hideLandingSection = document.getElementById("landingSection");
         hideLandingSection.style.display = "none";
         showDeleteSection.style.display = "flex";
-    })
+    
 }
 
 // função criada para retornar à landing page. Feitar para servir nas três ou mais possibilidades de aparição
@@ -46,21 +46,21 @@ function returnLandingPage(){
     const returnFromCreateBtn = document.getElementById("returnBtnFromCreate");
     const toLandingPageBtn = document.getElementById("returnBtnFromCreate");
 
-    returnFromCreateBtn.addEventListener("click", ()=>{
-            hideCreateSection.style.display = "none";
-            showLandingPage.style.display = "flex";
-    })
+    
+    hideCreateSection.style.display = "none";
+    showLandingPage.style.display = "flex";
+   
 
     const returnFromListBtn = document.getElementById("returnBtnFromList");
-    returnFromListBtn.addEventListener("click", ()=>{
-            hideListSection.style.display = "none";
-            showLandingPage.style.display = "flex";
-    })
+    
+    hideListSection.style.display = "none";
+    showLandingPage.style.display = "flex";
+    
 
     const returnFromDeleteBtn = document.getElementById("returnBtnFromDelete");
-    returnFromDeleteBtn.addEventListener("click", ()=>{
-            hideDeleteSection.style.display = "none";
-            showLandingPage.style.display = "flex";
-    })
+    
+    hideDeleteSection.style.display = "none";
+    showLandingPage.style.display = "flex";
+    
 }
 
