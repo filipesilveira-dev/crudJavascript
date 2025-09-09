25f20825345c7ed539b1c782f345e2d88ac4065e
Filipe <filipesilveira.dev@gmail.com>
2025-09-09 11:06:05 -0300

    Alteração de endpoint

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function returnLandingPage(){
 }
 
     //fetch para pegar (GET) as informações salvas na api do crudcrud
-fetch("https://crudcrud.com/api/07feaa325c3d42349fb3051dfc2fdbf9/user")
+fetch("https://crudcrud.com/api/6d807f61ce084155825b1f256a47ba98/user")
     //caso a requisição dê certo, ou seja, a resposta venha, visa-se buscar o json dela
     .then((response) => response.json())
     //se der certo pegar o json da resposta, será retornada uma array com vários usuários e seus emails
@@ -88,7 +88,7 @@ fetch("https://crudcrud.com/api/07feaa325c3d42349fb3051dfc2fdbf9/user")
 
     
     //a URL será a mesma utilizada anteriormente no método GET, porém ao final será acrecentado um objeto e suas informações que serão acrescidas
-    fetch("https://crudcrud.com/api/07feaa325c3d42349fb3051dfc2fdbf9/user", {
+    fetch("https://crudcrud.com/api/6d807f61ce084155825b1f256a47ba98/user", {
 
         method: "POST", //método de acrescentar
         headers: {
@@ -122,7 +122,7 @@ fetch("https://crudcrud.com/api/07feaa325c3d42349fb3051dfc2fdbf9/user")
 
 // função criada para deletar um usuário da API
 function remove(userId) {
-    fetch(`https://crudcrud.com/api/07feaa325c3d42349fb3051dfc2fdbf9/user/${userId}`, {
+    fetch(`https://crudcrud.com/api/6d807f61ce084155825b1f256a47ba98/user/${userId}`, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json"
